1e390c7e0646d58c36b5e0ab2d55f87abcd01812
shiinedev <shiinecadde17@gmail.com>
2026-04-02 09:46:36 +0100

    fix schema package error

--- apps/api/src/routes/v1/food.routes.ts
+++ apps/api/src/routes/v1/food.routes.ts
@@ -6,13 +6,20 @@ import {
 } from "@/controller/food.controller.js";
 import { requireToken } from "@/middleware/auth.js";
 import { upload } from "@/middleware/upload.js";
+import { validateSchema } from "@/middleware/validate-schema.js";
+import { saveFoodEntrySchema } from "@calorie-track/schemas/foodSchema";
 import { Router } from "express";
 
 const foodRoutes = Router();
 
 foodRoutes.post("/scan", requireToken, upload.single("image"), scanFood);
 foodRoutes.post("analyze", requireToken, upload.single("image"), analyzeImage);
-foodRoutes.post("/save", requireToken, saveFoodEntry);
+foodRoutes.post(
+  "/save",
+  requireToken,
+  validateSchema(saveFoodEntrySchema),
+  saveFoodEntry,
+);
 foodRoutes.post("/discard", requireToken, discardAnalyzedFood);
 
 export default foodRoutes;
