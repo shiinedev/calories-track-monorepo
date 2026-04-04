be9161e38d3acac8d09a3c60ef612aabaef52c96
shiinedev <shiinecadde17@gmail.com>
2026-04-04 14:32:06 +0100

    added try catch in startServer function

--- apps/api/src/server.ts
+++ apps/api/src/server.ts
@@ -3,13 +3,22 @@ import { connectDB } from "@/config/db";
 import { log } from "evlog";
 
 export default async function startServer() {
-  connectDB();
-  app.listen(process.env.PORT || 9000, () => {
-    log.info({
-      message: "Server started",
-      port: process.env.PORT || 9000,
+  try {
+    connectDB();
+    app.listen(process.env.PORT || 9000, () => {
+      log.info({
+        message: "Server started",
+        port: process.env.PORT || 9000,
+      });
     });
-  });
+  } catch (error) {
+    log.error({
+      message: "failed to connect database",
+      couse: "database or port error",
+    });
+
+    throw error;
+  }
 }
 
 startServer();
