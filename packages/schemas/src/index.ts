31133267fd5f8fffce0fa49aaacc10a319c8acaa
shiinedev <shiinecadde17@gmail.com>
2026-04-06 16:37:41 +0100

    updated schema package

---
+++ packages/schemas/src/index.ts
@@ -0,0 +1,13 @@
+export {
+  loginSchema,
+  registerSchema,
+  type LoginSchema,
+  type RegisterSchema,
+} from "./auth.schema";
+export {
+  FoodAnalysisSchema,
+  MealTypeSchema,
+  saveFoodEntrySchema,
+  type FoodAnalysisResult,
+  type SaveFoodEntryResult,
+} from "./food.schema";
